1b2a59b681d155bf3c007c0164106ef8ff6a1f66
Purushothaman Chinnappan <purushothaman.c@itp.objectfrontier.com>
2023-08-21 16:21:15 +0530

    Modified by Purushoth

--- MyClass.java
+++ MyClass.java
@@ -1,3 +1,15 @@
 class MyClass {
 
+public static void Add(int a,int b) {
+   Sum=a+b;
+System.out.println("The value of sum is "+Sum);
+}
+
+public static void Subtract(int a,int b) {
+   Sub=a-b;
+System.out.println("The value of subtract is "+Sub);
+}
+
+// Add & Sub added by Purushoth
+
 }
