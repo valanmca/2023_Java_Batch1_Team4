351ba8963fecca7cc1199b6a67298cd5254aec24
Nagajeyandhan Ravi <nagajeyandhan.ravi@itp.objectfrontier.com>
2023-08-21 16:19:01 +0530

    First modification from jeyandhan

--- MyClass.java
+++ MyClass.java
@@ -1,3 +1,16 @@
 class MyClass {
+    void age() {
+        System.out.println("Age is 23");
 
+    }
+
+    void name() {
+        System.out.println("Name: Jeyandhan");
+    }
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.age();
+        obj.name();
+    }
 }
